e7714122d40623e58709c14699ee00bc52f15596
Caleb Tracey <caleb.tracey@gmail.com>
2022-02-21 16:32:23 -0500

    express update for heroku

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 8080
-app.use(express.static(`${__dirname}/`))
-app.listen(process.env.PORT || 8080)
 
 app.use(express.static(path.join(__dirname, 'build')))
 
