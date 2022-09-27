a3404260fb38487be257c8add3a0ba183216326b
dtracey <dominic.tracey@gmail.com>
2022-09-27 17:18:28 -0400

    local port fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = process.env.PORT || 6088
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'build')))
 
